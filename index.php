1143296eced7e0f3a45d0bb9a35253179c14cdda
Tony Ventura <tonnycv007@gmail.com>
2026-03-03 17:55:42 -0600

    cambio 1

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <html lang="es">
 <head>
   <meta charset="UTF-8">
-  <title><?= $appName ?> | Bienvenido</title>
+  <title><?= $appName ?> | Bienvenido Profe</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <style>
